ed14dd261efaa4b7230d12718dccea324783589b
Michael Bell <michaelrbell@ymail.com>
2024-05-31 11:41:11 +0100

    Add log type

---
+++ utils/types/log.go
@@ -0,0 +1,50 @@
+package types
+
+import "log/slog"
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+type LogType int
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+const (
+	LogTypeRequest LogType = iota
+	LogTypeCron
+	LogTypeCourseScanner
+	LogTypeFileSystem
+	LogTypeDB
+)
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// AllLogTypes returns a slice of all log types in string form
+func AllLogTypes() []string {
+	return []string{
+		LogTypeRequest.String(),
+		LogTypeCron.String(),
+		LogTypeCourseScanner.String(),
+		LogTypeFileSystem.String(),
+		LogTypeDB.String(),
+	}
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// String returns the string representation of the LogType
+func (lt LogType) String() string {
+	names := [...]string{"request", "cron", "course scanner", "file system", "db"}
+
+	if int(lt) < 0 || int(lt) >= len(names) {
+		return "unknown"
+	}
+
+	return names[lt]
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// LogValue implements the slog.Value interface
+func (lt LogType) LogValue() slog.Value {
+	return slog.StringValue(lt.String())
+}
---
+++ utils/types/log_test.go
@@ -0,0 +1,58 @@
+package types
+
+import (
+	"log/slog"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+func TestLog_String(t *testing.T) {
+	tests := []struct {
+		logType  LogType
+		expected string
+	}{
+		{LogTypeRequest, "request"},
+		{LogTypeCron, "cron"},
+		{LogTypeCourseScanner, "course scanner"},
+		{LogTypeFileSystem, "file system"},
+		{LogTypeDB, "db"},
+		{LogType(999), "unknown"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.expected, func(t *testing.T) {
+			require.Equal(t, tt.expected, tt.logType.String())
+		})
+	}
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+func TestLog_AllLogTypes(t *testing.T) {
+	expected := []string{"request", "cron", "course scanner", "file system", "db"}
+	require.Equal(t, expected, AllLogTypes())
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+func TestLog_LogValue(t *testing.T) {
+	tests := []struct {
+		logType  LogType
+		expected string
+	}{
+		{LogTypeRequest, "request"},
+		{LogTypeCron, "cron"},
+		{LogTypeCourseScanner, "course scanner"},
+		{LogTypeFileSystem, "file system"},
+		{LogTypeDB, "db"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.expected, func(t *testing.T) {
+			require.Equal(t, slog.StringValue(tt.expected), tt.logType.LogValue())
+		})
+	}
+}
