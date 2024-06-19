9bd9434e4974d06b87e95f4e90338aebc38f902a
Michael Bell <michaelrbell@ymail.com>
2024-06-19 15:37:29 +0100

    Remove unneeded import

--- ui/src/lib/components/pages/settings_course_details/index.ts
+++ ui/src/lib/components/pages/settings_course_details/index.ts
@@ -1,3 +1,2 @@
-export { default as CourseDetailsCard } from './card.svelte';
 export { default as CourseDetailsChapters } from './chapters.svelte';
 export { default as CourseDetailsTags } from './tags.svelte';
