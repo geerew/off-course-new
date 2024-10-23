13349d6c76a9ff27e06dd3c625ca884fba072c0d
Michael Bell <michaelrbell@ymail.com>
2024-10-23 15:21:43 +0100

    Additional errors

--- utils/errors.go
+++ utils/errors.go
@@ -8,8 +8,13 @@ var (
 	ErrNilPtr       = errors.New("nil pointer")
 	ErrNotPtr       = errors.New("requires a pointer")
 	ErrNotModeler   = errors.New("does not implement Modeler interface")
-	ErrEmbedded     = errors.New("embedded struct does not implement Fielder interface")
+	ErrEmbedded     = errors.New("embedded struct does not implement Definer interface")
 	ErrInvalidValue = errors.New("invalid value")
+	ErrNotStruct    = errors.New("not a struct")
+	ErrNotSlice     = errors.New("not a slice")
+
+	// DB
+	ErrInvalidWhere = errors.New("where clause cannot be empty")
 
 	// Model errors
 	ErrInvalidId       = errors.New("id cannot be empty")
