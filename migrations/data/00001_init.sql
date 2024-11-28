5fa933760b4d4d662e53dba6c73417cc4718837d
Michael Bell <michaelrbell@ymail.com>
2024-11-28 11:35:42 +0000

    Rework sql (breaking)

--- migrations/data/00001_init.sql
+++ migrations/data/00001_init.sql
@@ -45,28 +45,24 @@ CREATE TABLE assets (
 CREATE TABLE assets_progress (
 	id           TEXT PRIMARY KEY NOT NULL,
 	asset_id     TEXT NOT NULL UNIQUE,
-	course_id    TEXT NOT NULL,
 	video_pos    INTEGER NOT NULL DEFAULT 0,
 	completed	 BOOLEAN NOT NULL DEFAULT FALSE,
 	completed_at TEXT,
 	created_at   TEXT NOT NULL DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),
 	updated_at   TEXT NOT NULL DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),
 	---
-	FOREIGN KEY (course_id) REFERENCES courses (id) ON DELETE CASCADE,
 	FOREIGN KEY (asset_id) REFERENCES assets (id) ON DELETE CASCADE
 );
 
 --- Attachments information
 CREATE TABLE attachments (
 	id          TEXT PRIMARY KEY NOT NULL,
-	course_id   TEXT NOT NULL,
 	asset_id    TEXT NOT NULL,
 	title       TEXT NOT NULL,
 	path        TEXT UNIQUE NOT NULL,
 	created_at  TEXT NOT NULL DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),
 	updated_at  TEXT NOT NULL DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),
 	---
-	FOREIGN KEY (course_id) REFERENCES courses (id) ON DELETE CASCADE,
 	FOREIGN KEY (asset_id) REFERENCES assets (id) ON DELETE CASCADE
 );
 
@@ -74,7 +70,7 @@ CREATE TABLE attachments (
 CREATE TABLE scans (
 	id          TEXT PRIMARY KEY NOT NULL,
 	course_id   TEXT UNIQUE NOT NULL,
-	status      TEXT NOT NULL,
+    status      TEXT NOT NULL DEFAULT 'waiting',
 	created_at  TEXT NOT NULL DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),
 	updated_at  TEXT NOT NULL DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),
 	---
@@ -101,4 +97,30 @@ CREATE TABLE courses_tags (
 	FOREIGN KEY (course_id) REFERENCES courses (id) ON DELETE CASCADE,
 	---
 	CONSTRAINT unique_course_tag UNIQUE (tag_id, course_id)
+);
+
+--- Parameters table for application settings
+CREATE TABLE params (
+    id           TEXT PRIMARY KEY NOT NULL,
+    key          TEXT UNIQUE NOT NULL,
+    value        TEXT NOT NULL,
+    created_at   TEXT NOT NULL DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),
+    updated_at   TEXT NOT NULL DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW'))
+);
+
+-- Insert initial parameter to track admin creation
+INSERT INTO params (id, key, value) VALUES (
+    'wV0418r0Rr',
+    'hasAdmin',
+    'false'
+);
+
+--- User information
+CREATE TABLE users (
+    id            TEXT PRIMARY KEY NOT NULL,
+    username      TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    role          TEXT NOT NULL CHECK(role IN ('admin', 'user')),
+    created_at    TEXT NOT NULL DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),
+    updated_at    TEXT NOT NULL DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW'))
 );
