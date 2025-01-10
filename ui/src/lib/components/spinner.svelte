2775804c1ba1b967905125be79086cce3abb9012
Michael Bell <michaelrbell@ymail.com>
2025-01-10 16:41:47 +0000

    ui: rework the spinner

--- ui/src/lib/components/spinner.svelte
+++ ui/src/lib/components/spinner.svelte
@@ -1,38 +1,26 @@
 <script lang="ts">
 	import { cn } from '$lib/utils';
 
-	let { class: className = '' } = $props();
+	let { class: componentClass }: { class?: string } = $props();
 </script>
 
-<span
-	class={cn(
-		'loader text-foreground-alt-2 relative size-[2.5em] rounded-full text-base text-[10px] before:absolute before:top-0 before:-left-[3.5em] before:size-[2.5em] before:rounded-full after:absolute after:top-0 after:left-[3.5em] after:size-[2.5em] after:rounded-full',
-		className
-	)}
-></span>
-
-<style>
-	.loader,
-	.loader:before,
-	.loader:after {
-		animation: bblFadInOut 1.5s infinite ease-in-out;
-	}
-	.loader {
-		animation-delay: -0.16s;
-	}
-
-	.loader:before {
-		animation-delay: -0.32s;
-	}
-
-	@keyframes bblFadInOut {
-		0%,
-		80%,
-		100% {
-			box-shadow: 0 2.5em 0 -1.3em;
-		}
-		40% {
-			box-shadow: 0 2.5em 0 0;
-		}
-	}
-</style>
+<div class="flex flex-row items-center gap-2">
+	<div
+		class={cn(
+			'animate-flash size-[2.5em] rounded-full bg-red-400 delay-[-0.32s] duration-1500 ease-in-out',
+			componentClass
+		)}
+	></div>
+	<div
+		class={cn(
+			'animate-flash size-[2.5em] rounded-full bg-red-400 delay-[-0.16s] duration-1500 ease-in-out',
+			componentClass
+		)}
+	></div>
+	<div
+		class={cn(
+			'animate-flash size-[2.5em] rounded-full bg-red-400 duration-1500 ease-in-out',
+			componentClass
+		)}
+	></div>
+</div>
--- ui/src/routes/(main)/+layout.svelte
+++ ui/src/routes/(main)/+layout.svelte
@@ -13,7 +13,7 @@
 	<Error message={'Failed to fetch user: ' + auth.error} />
 {:else if auth.user === null}
 	<div class="flex w-full justify-center pt-14 sm:pt-20">
-		<Spinner />
+		<Spinner class="bg-foreground-alt-2 size-6" />
 	</div>
 {:else}
 	<div class="px-4 pb-8 lg:px-8">
